45d7a8d6f1d7f49c3fa48a7592cb928b45816a97
free <xuchen@elastos.org>
2018-06-06 14:43:00 +0800

    add mnemonicList page

--- src/pages/mnemonic/mnemonic.component.ts
+++ src/pages/mnemonic/mnemonic.component.ts
@@ -11,28 +11,15 @@ import {WriteComponent} from "./write/write.component";
 export class MnemonicComponent extends BaseComponent implements OnInit {
 
 
-  mnemonicList = [
-    {text: '醉', select: true},
-    {text: '酒', select: true},
-    {text: '当', select: true},
-    {text: '歌', select: true},
-    {text: '人', select: true},
-    {text: '生', select: true},
-    {text: '几', select: true},
-    {text: '和', select: true},
-    {text: '哎', select: true},
-    {text: '呦', select: true},
-    {text: '呦', select: true},
-    {text: '呦', select: true},
-  ];
+  mnemonicList = [];
 
   ngOnInit() {
     this.setTitleByAssets('text-mnemonic');
-    // this.storage.getWallet((data: WalletModel) => {
-    //   this.walletManager.exportWalletWithMnemonic(this.walletData.backupPassword, (res) => {
-    //     this.walletData.mnemonic = res.mnemonic;
-    //   });
-    // });
+    this.localStorage.getWallet().then((val)=>{
+      this.walletManager.exportWalletWithMnemonic(val.backupPassword, (res) => {
+        this.mnemonicList = res.mnemonic;
+      });
+    });
   }
 
   onNext() {
