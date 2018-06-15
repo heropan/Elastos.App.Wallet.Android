a1c3db3128740f77294046e1986715ce21663635
free1 <xuchen@elastos.org>
2018-06-15 14:50:39 +0800

    [coin list page] add sub wallet coin list

--- src/pages/coin/coin-list/coin-list.component.ts
+++ src/pages/coin/coin-list/coin-list.component.ts
@@ -8,18 +8,25 @@ import {BaseComponent} from './../../../app/BaseComponent';
 })
 export class CoinListComponent extends BaseComponent implements OnInit {
 
-  coinList = [
-    {name: 'ELA', open: true},
-    {name: 'BTC', open: false}
-  ];
+  coinList = [];
 
   onSelect(item) {
     item.open = ! item.open;
   }
 
-
   ngOnInit() {
     this.setTitleByAssets('text-coin-list');
+    this.walletManager.getSupportedChains((allChains) => {
+      // {"ELA": "ELA", "ID": "ID"}
+      for (var i = 0; i < allChains.length; i++) {
+        let isOpen = false;
+        this.walletManager.getAllChainIds((openChains)=>{
+          isOpen = allChains[i] in openChains ? true : false;
+        });
+        this.coinList.push({name: allChains[i], open: isOpen});
+      }
+      // this.coinList = [{name: 'ELA', open: true}, {name: 'BTC', open: false}];
+    });
   }
 
 }
