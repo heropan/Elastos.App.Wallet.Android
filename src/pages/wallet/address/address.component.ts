63d1da131a1964905aadbe57a4ac4e894ea0f3ee
songjiazhun <869077613@qq.com>
2018-06-26 03:50:26 -0400

    地址页面

--- src/pages/wallet/address/address.component.ts
+++ src/pages/wallet/address/address.component.ts
@@ -4,18 +4,16 @@ import {BaseComponent} from './../../../app/BaseComponent';
 @Component({
   selector: 'app-address',
   templateUrl: './address.component.html',
-  // styleUrls: ['./address.component.scss']
 })
 export class AddressComponent extends BaseComponent implements OnInit {
 
   addrList = [];
-  chianId: string;
-
+  chinaId: string;
   ngOnInit() {
     this.setTitleByAssets('text-contacts-address');
-    this.chianId = this.getNavParams().get("chianId");
-    this.walletManager.getAllAddress(this.chianId, 0, (data) => {
-      this.addrList = data['Addresses'];
+    this.chinaId = this.getNavParams().get("chinaId");
+    this.walletManager.getAllAddress(this.chinaId, 0, (data) => {
+      this.addrList = JSON.parse(data)['Addresses'];
     });
   }
 
