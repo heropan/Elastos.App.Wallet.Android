6fccb2a15d6dff35fbdbec3c8d6df2d807564bc8
wanli <zhaowanli@elastos.org>
2018-06-27 22:53:09 +0800

    Fixed the running error about GetAllKeys.

--- platforms/android/app/src/main/jni/ElaDid.cpp
+++ platforms/android/app/src/main/jni/ElaDid.cpp
@@ -56,7 +56,18 @@ static /*nlohmann::json*/ jstring JNICALL nativeGetAllKeys(JNIEnv *env, jobject 
 {
     IDID* did = (IDID*)jDidProxy;
     nlohmann::json jsonValue = did->GetAllKeys(jstart, jcount);
-    LOGD("FUNC=[%s]========================value=[%s]", __FUNCTION__, ToStringFromJson(jsonValue));
+    const char* value = ToStringFromJson(jsonValue);
+    if (value != NULL) {
+        unsigned char firstCh = value[0];
+        LOGD("FUNC=[%s]========================value=[%s], firstCh=[0x%02x]", __FUNCTION__, value, firstCh);
+        std::stringstream ss;
+        ss << jsonValue;
+        jstring retValue = stringTojstring(env, ss.str());
+        LOGD("FUNC=[%s]========================LINE=[%d], ", __FUNCTION__, __LINE__);
+        return retValue;
+    }
+
+    LOGD("FUNC=[%s]========================value=[%s], jstart=[%d], jcount=[%d]", __FUNCTION__, value, jstart, jcount);
     return env->NewStringUTF(ToStringFromJson(jsonValue));
 }
 
