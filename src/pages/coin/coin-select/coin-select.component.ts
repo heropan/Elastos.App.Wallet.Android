9cc221b4be742ab5170640113fe6933ed8d820cd
free1 <xuchen@elastos.org>
2018-07-01 20:08:05 +0800

    [coin select page] add coin list show

--- src/pages/coin/coin-select/coin-select.component.ts
+++ src/pages/coin/coin-select/coin-select.component.ts
@@ -13,70 +13,22 @@ export class CoinSelectComponent extends BaseComponent implements OnInit {
   @ViewChild('subscribe') subPopup: PopupComponent;
 
   coinList = [];
-  coinListCache = {};
-  payPassword: string;
-  singleAddress: boolean = false;
-  currentCoin: string;
-
-  onSelect(item) {
-    item.open = ! item.open;
-    if (item.open) {
-      this.currentCoin = item.name;
-      this.subPopup.show().subscribe((res: boolean) => {
-      });
-    } else {
-      this.localStorage.get('coinListCache').then((val)=>{
-        let coinListCache = JSON.parse(val);
-        delete(coinListCache[item.name]);
-        this.localStorage.set('coinListCache', coinListCache);
-      });
-    }
-  }
 
   ngOnInit() {
     this.setTitleByAssets('text-coin-list');
-    // this.setLeftIcon("",()=>{
-    //   this.events.publish("home:update");
-    //   this.Back();
-    // });
-    // this.localStorage.get('coinListCache').then((val)=>{
-    //   // this.walletManager.getSupportedChains((allChains) => {
-    //     let allChains = [1,3]
-    //     for (var chain in allChains) {
-    //       let isOpen = false;
-    //       let coinListCache = JSON.parse(val);
-    //       if (coinListCache) {
-    //         isOpen = chain in coinListCache ? true : false;
-    //       }
-    //       if (chain == "ELA") {
-    //         isOpen = true;
-    //       }
-    //       this.coinList.push({name: chain, open: isOpen});
-    //     }
-    //   // });
-    // });
-  }
-
-  onClick() {
-    this.createSubWallet(this.currentCoin);
-  }
-
-  createSubWallet(chainId){
-    // Sub Wallet IdChain
-    this.walletManager.createSubWallet(chainId, this.payPassword, this.singleAddress, 0, (val)=>{
-      if (val['ERRORCODE'] == undefined) {
-        if (!Util.password(this.payPassword)) {
-          this.toast("text-pwd-validator");
-          return;
+    this.localStorage.get('coinListCache').then((val)=>{
+      let coinListCache = JSON.parse(val);
+      this.coinList = [];
+      for (let coin in coinListCache) {
+        if (coin != 'ELA') {
+          this.coinList.push({name: coin});
         }
-        let coin = {};
-        coin["id"] = chainId;
-        this.localStorage.add('coinListCache', coin);
-        this.subPopup.hide();
-      }else{
-        this.toast("text-password-error");
       }
     });
+  }
+
+  onItem(item) {
+    this.Go(RechargeComponent, {chianId: item.name});
   }
 
 }
