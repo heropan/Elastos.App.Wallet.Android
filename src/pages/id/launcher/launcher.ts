3828c31bfb8a66bb1817a6852be832a9761d2812
houpei <houshoupei@126.com>
2018-08-11 20:29:19 +0800

    solve conflicts

--- src/pages/id/launcher/launcher.ts
+++ src/pages/id/launcher/launcher.ts
@@ -2,7 +2,8 @@ import { Component,OnInit } from '@angular/core';
 import {BaseComponent} from "../../../app/BaseComponent";
 import {IdImportComponent} from "../../../pages/id/import/import";
 import {IdHomeComponent} from "../../../pages/id/home/home";
-import { Config } from '../../../providers/Config';
+import {Config} from "../../../providers/Config";
+
 @Component({
   selector: 'id-launcher',
   templateUrl: 'launcher.html',
@@ -29,23 +30,32 @@ export class IdLauncherComponent extends BaseComponent implements OnInit{
 
           let idObj ={id:result.didname};
 
+
           console.info("ElastosJs luncher.ts createDID result add registerIdListener" + JSON.stringify(result));
           self.walletManager.registerIdListener(result.didname, (data) => {
 
-            alert("luncher.ts ElastosJs  createDID registerIdListener  data  callback"+ JSON.stringify(data));
-            console.info("luncher.ts ElastosJs createDID registerIdListener  data  callback !!!!!" + JSON.stringify(data));
-
+            console.info("home.ts ElastosJs createDID registerIdListener "+ JSON.stringify(data));
+            //alert("home.ts createDID registerIdListener  data  callback"+ JSON.stringify(data));
+            //first commit
             if(data["path"] == "Added"){
 
-              if((data["Contents"].length > 0) && data["Contents"][0]["Proof"]){
+              let valueObj = JSON.parse(data["value"]) ;
+              if((valueObj["Contents"].length > 0) && valueObj["Contents"][0]["Proof"]){
 
-                let proofObj = JSON.parse(data["Contents"][0]["Proof"]);
+                let proofObj = JSON.parse(valueObj["Contents"][0]["Proof"]);
+
+                console.info("home.ts ElastosJs createDID proofObj[\"signature\"]  "+ proofObj["signature"]);
                 let seqNumObj = self.dataManager.getSeqNumObj(proofObj["signature"]);
+
                 let serialNum =  seqNumObj["serialNum"] ;
                 console.info("home.ts ElastosJs createDID serialNum "+ serialNum);
-                this.setOrderStatus(3,serialNum);
+                self.setOrderStatus(3,serialNum);
               }
             }
+            alert("home.ts createDID registerIdListener  data  callback"+ JSON.stringify(data));
+            //console.info("home.ts ElastosJs createDID registerIdListener " + JSON.stringify(data));
+
+            console.info("home.ts ElastosJs createDID registerIdListener  data  callback !!!!!" + JSON.stringify(data));
 
 
           });
@@ -57,21 +67,34 @@ export class IdLauncherComponent extends BaseComponent implements OnInit{
 
 
   setOrderStatus(status,serialNum){
+
+    console.info("setOrderStatus begin status " + status +" serialNum " + serialNum);
+
     let serids = Config.getSerIds();
     let serid = serids[serialNum];
+
+    console.info("setOrderStatus serid" + JSON.stringify(serid));
+    console.info("setOrderStatus serids" + JSON.stringify(serids));
+
     let did = serid["id"];
     let appName = serid["appName"];
     let appr = serid["appr"];
+
+    console.info("setOrderStatus appr" + appr);
+
     let idsObj = {};
     this.localStorage.getKycList("kycId").then((val)=>{
-        if(val == null || val === undefined || val === {} || val === ''){
-             return;
-        }
-     idsObj = JSON.parse(val);
-     idsObj[did][appName][appr]["order"][serialNum]["status"] = status;
-     this.localStorage.set("kycId",idsObj).then(()=>{
+      if(val == null || val === undefined || val === {} || val === ''){
+        return;
+      }
+      idsObj = JSON.parse(val);
+      idsObj[did][appName][appr]["order"][serialNum]["status"] = status;
+      this.localStorage.set("kycId",idsObj).then(()=>{
 
-     });
+      });
     });
   }
+
 }
+
+
