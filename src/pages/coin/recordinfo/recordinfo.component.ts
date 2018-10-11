0601481b70066b33707b104db48ee87003d0409f
songjiazhun <869077613@qq.com>
2018-10-11 08:03:38 -0400

    set recordinfo

--- src/pages/coin/recordinfo/recordinfo.component.ts
+++ src/pages/coin/recordinfo/recordinfo.component.ts
@@ -21,53 +21,58 @@ export class RecordinfoComponent extends BaseComponent implements OnInit {
     this.setTitleByAssets('text-record');
     let txId = this.getNavParams().get("txId");
     let chainId = this.getNavParams().get("chainId");
-    // this.walletManager.getAllTransaction(this.masterWalletId,chainId, this.start, txId, (data) => {
-    //   if(data["success"]){
-    //     console.log("====getAllTransaction====="+JSON.stringify(data));
-    //     let allTransaction = JSON.parse(data['success']);
-    //     let transactions = allTransaction['Transactions'];
-    //     let transaction = transactions[0];
-    //     let timestamp = transaction['Timestamp']*1000;
-    //     let datetime = Util.dateFormat(new Date(timestamp), 'yyyy-MM-dd hh:mm:ss');
-    //     let summary = transaction['Summary'];
-    //     let incomingAmount = summary["Incoming"]['Amount'];
-    //     let outcomingAmount = summary["Outcoming"]['Amount'];
-    //     let incomingAddress = summary["Incoming"]['ToAddress'];
-    //     let outcomingAddress = summary["Outcoming"]['ToAddress'];
-    //     let balanceResult = incomingAmount - outcomingAmount;
-    //     let status = '';
-    //     switch(summary["Status"])
-    //     {
-    //       case 'Confirmed':
-    //         status = 'Confirmed'
-    //         break;
-    //       case 'Pending':
-    //         status = 'Pending'
-    //         break;
-    //       case 'Unconfirmed':
-    //         status = 'Unconfirmed'
-    //         break;
-    //     }
-    //     this.transactionRecord = {
-    //       name: chainId,
-    //       status: status,
-    //       balance: balanceResult/Config.SELA,
-    //       incomingAmount: incomingAmount/Config.SELA,
-    //       outcomingAmount: outcomingAmount/Config.SELA,
-    //       incomingAddress: incomingAddress,
-    //       outcomingAddress: outcomingAddress,
-    //       txId: txId,
-    //       transactionTime: datetime,
-    //       timestamp: timestamp,
-    //       payfees: summary['Fee']/Config.SELA,
-    //       confirmCount: summary["ConfirmStatus"],
-    //       remark: summary["Remark"]
-    //     }
-    //   }else{
-    //       alert("======getAllTransaction====error"+JSON.stringify(data));
-    //   }
+    this.walletManager.getAllTransaction(this.masterWalletId,chainId, this.start, txId, (data) => {
+      if(data["success"]){
+        console.log("====getAllTransaction====="+JSON.stringify(data));
+        let allTransaction = JSON.parse(data['success']);
+        let transactions = allTransaction['Transactions'];
+        let transaction = transactions[0];
+        let timestamp = transaction['Timestamp']*1000;
+        let datetime = Util.dateFormat(new Date(timestamp), 'yyyy-MM-dd hh:mm:ss');
+        let summary = transaction['Summary'];
+        let incomingAmount = summary["Incoming"]['Amount'];
+        let outcomingAmount = summary["Outcoming"]['Amount'];
+        let incomingAddress = summary["Incoming"]['ToAddress'];
+        let outcomingAddress = summary["Outcoming"]['ToAddress'];
+        let balanceResult = incomingAmount - outcomingAmount;
+        let status = '';
+        switch(summary["Status"])
+        {
+          case 'Confirmed':
+            status = 'Confirmed'
+            break;
+          case 'Pending':
+            status = 'Pending'
+            break;
+          case 'Unconfirmed':
+            status = 'Unconfirmed'
+            break;
+        }
+        this.transactionRecord = {
+          name: chainId,
+          status: status,
+          balance: balanceResult/Config.SELA,
+          incomingAmount: incomingAmount/Config.SELA,
+          outcomingAmount: outcomingAmount/Config.SELA,
+          incomingAddress: incomingAddress,
+          outcomingAddress: outcomingAddress,
+          txId: txId,
+          transactionTime: datetime,
+          timestamp: timestamp,
+          payfees: summary['Fee']/Config.SELA,
+          confirmCount: summary["ConfirmStatus"],
+          remark: summary["Remark"]
+        }
+      }else{
+          alert("======getAllTransaction====error"+JSON.stringify(data));
+      }
 
-    // });
+    });
+  }
+
+  onNext(address){
+    this.native.copyClipboard(address);
+    this.toast('copy-ok');
   }
 
 }
