c57d9ad4ba29659815153724d983ea1db1aaa23f
songjiazhun <869077613@qq.com>
2018-11-13 22:48:17 -0500

    set [scancode page]

--- src/pages/scancode/scancode.ts
+++ src/pages/scancode/scancode.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {Native} from "../../providers/Native";
-
+import {WalletManager} from '../../providers/WalletManager'
+import { Config } from '../../providers/Config';
 
 @Component({
   selector: 'page-scancode',
@@ -9,8 +10,23 @@ import {Native} from "../../providers/Native";
 })
 export class ScancodePage {
   public qrcode: string=null;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public native: Native) {
-           this.qrcode = JSON.stringify(this.navParams.data);
+  public txHash:string ="";
+  public toAddress:string="";
+  public fee:any;
+  public amount:any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public native: Native,public walletManager: WalletManager) {
+           let params = this.navParams.data;
+           this.fee = params["tx"]["fee"];
+          let  txObj  = params["tx"]["raw"];
+           this.walletManager.decodeTransactionFromString(txObj,(raw)=>{
+            if(raw["success"]){
+                this.native.info(raw);
+                this.txHash = JSON.parse(raw["success"])["TxHash"];
+                this.toAddress =JSON.parse(raw["success"])["Outputs"][0]["Address"];
+                this.amount = JSON.parse(raw["success"])["Outputs"][0]["Amount"]/Config.SELA;
+            }
+         });
+           this.qrcode = JSON.stringify(params);
            this.native.info(this.navParams.data);
   }
 
