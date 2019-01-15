2315b21dff70fb895c0077adcd0d1f6b0a2105c7
songjiazhun <869077613@qq.com>
2019-01-14 21:25:38 -0500

    set [votemanage.ts]

--- src/pages/supernodeelection/superpoint/votemanage/votemanage.ts
+++ src/pages/supernodeelection/superpoint/votemanage/votemanage.ts
@@ -4,6 +4,7 @@ import {PopupProvider} from "../../../../providers/popup";
 import {WalletManager} from "../../../../providers/WalletManager";
 import {Native} from "../../../../providers/Native";
 import {Util} from '../../../../providers/Util';
+import {  Config } from '../../../../providers/Config';
 
 /**
  * Generated class for the VotemanagePage page.
@@ -19,9 +20,12 @@ import {Util} from '../../../../providers/Util';
 })
 export class VotemanagePage {
   public passworld:string = "";
-  public masterWalletId:string ="1";
-  public publicKey:string = "qwerttttt";
+  public masterId:string ="1";
+  public publickey:string = "";
+  public curChain = "ELA";
   constructor(public navCtrl: NavController, public navParams: NavParams,public popupProvider: PopupProvider,public native :Native,public walletManager :WalletManager) {
+    this.masterId = Config.getCurMasterWalletId();
+    this.getPublicKeyForVote();
   }
 
   ionViewDidLoad() {
@@ -51,4 +55,31 @@ export class VotemanagePage {
 
       // });
   }
+
+
+  getPublicKeyForVote(){
+    this.walletManager.getPublicKeyForVote(this.masterId,this.curChain,(data)=>{
+              this.native.info(data);
+              if(data["success"]){
+                this.publickey = data["success"];
+              }
+    });
+  }
+
+  generateCancelProducerPayload(){
+     this.walletManager.generateCancelProducerPayload(this.masterId,this.curChain,this.publickey,this.passworld,(data)=>{
+      this.native.info(data);
+      if(data["success"]){
+
+      }
+     });
+  }
+
+
+  createCancelProducerTransaction(payloadJson){
+       this.walletManager.createCancelProducerTransaction(this.masterId,this.curChain,"",payloadJson,"",false,(data)=>{
+
+       });
+  }
+
 }
